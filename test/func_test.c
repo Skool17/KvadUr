d4a04445f44d0cf6b6003b673166a10a33185d6f
Skool17 <semen9422@gmail.com>
2017-06-23 02:32:23 +0700

    Create func_test.c

---
+++ test/func_test.c
@@ -0,0 +1,21 @@
+#include <ctest.h>
+#include <../src/diskr.h>
+
+// Discriminant
+CTEST(Test_1_function, input_2_4_2_0)
+{
+    float result = function(2,4,2,0);
+	
+    float expected = -1;
+	
+    ASSERT_DBL_NEAR(expected, result);
+}
+
+CTEST(Test_2_function, input_3_6_3_0)
+{
+    float result = function(3,6,3,0);
+    
+    float expected = -1;
+    
+    ASSERT_DBL_NEAR(expected, result);
+}
